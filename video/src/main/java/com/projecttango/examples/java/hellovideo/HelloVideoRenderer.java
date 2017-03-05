4500a52cb926b9df10d56ce01c60fb211fa36cda
davidytag <dytag@cs.uchicago.edu>
2017-03-04 21:49:30 -0600

    screenshots

--- video/src/main/java/com/projecttango/examples/java/hellovideo/HelloVideoRenderer.java
+++ video/src/main/java/com/projecttango/examples/java/hellovideo/HelloVideoRenderer.java
@@ -164,6 +164,29 @@ public class HelloVideoRenderer implements GLSurfaceView.Renderer {
 
         // Enable depth write again for any additional rendering on top of the camera surface
         GLES20.glDepthMask(true);
+
+        Bitmap img = SavePixels(0, 0, 1920, 1104, gl10);
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        img.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
+
+        //you can create a new file name "test.jpg" in sdcard folder.
+        File f = new File("/storage/emulated/0/Android/data/com.projecttango.examples.java.hellovideo/files/Pictures"
+                + File.separator + "ICU_test.jpg");
+
+        try {
+            if (!f.exists()) {
+                f.createNewFile();
+            }
+
+            //write the bytes in file
+            FileOutputStream fo = new FileOutputStream(f, false);
+            fo.write(bytes.toByteArray());
+
+            // remember close de FileOutput
+            fo.close();
+        } catch (IOException e) {
+            System.out.println("Could not write!");
+        }
     }
 
     private void createTextures() {
@@ -245,6 +268,31 @@ public class HelloVideoRenderer implements GLSurfaceView.Renderer {
             return 0;
         }
         return shader;
+    }
+
+    public static Bitmap SavePixels(int x, int y, int w, int h, GL10 gl)
+    {
+        int b[]=new int[w*(y+h)];
+        int bt[]=new int[w*h];
+        IntBuffer ib=IntBuffer.wrap(b);
+        ib.position(0);
+        gl.glReadPixels(x, 0, w, y+h, GL10.GL_RGBA, GL10.GL_UNSIGNED_BYTE, ib);
+
+        for(int i=0, k=0; i<h; i++, k++)
+        {//remember, that OpenGL bitmap is incompatible with Android bitmap
+            //and so, some correction need.
+            for(int j=0; j<w; j++)
+            {
+                int pix=b[i*w+j];
+                int pb=(pix>>16)&0xff;
+                int pr=(pix<<16)&0x00ff0000;
+                int pix1=(pix&0xff00ff00) | pr | pb;
+                bt[(h-k-1)*w+j]=pix1;
+            }
+        }
+
+        Bitmap sb=Bitmap.createBitmap(bt, w, h, Bitmap.Config.ARGB_8888);
+        return sb;
     }
 
     public int getTextureId() {
